0429bb4ad4855a2ab43f7a07c6b0d41fb31e45a6
gavin771 <gavin.samuels10@gmail.com>
2018-03-11 23:03:58 -0700

    created a dashboard layout

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
+import { Container, Dropdown, Header, Menu, Icon, Divider } from 'semantic-ui-react'
 
 class Dashboard extends Component {
   render () {
     return (
       <div>
-        Dashboard
+        <Menu fixed='top' size='large'>
+          <Container>
+            <Menu.Item as='a' header>
+              <Icon
+                inverted
+                circular
+                size='large'
+                name='tasks'
+                color='green'
+              />
+              SheetRail
+            </Menu.Item>
+            <Menu.Item name='settings' as='a' />
+            <Menu.Menu position='right'>
+              <Menu.Item name='logout' as='a' />
+            </Menu.Menu>
+          </Container>
+        </Menu>
+
+        <Container text style={{ marginTop: '7em' }} textAlign='center'>
+          <Header as='h1'>Control Center</Header>
+          <Divider />
+        </Container>
       </div>
     )
   }
