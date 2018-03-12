e4178ab6fd8bd772a47e2adef1e5698e42e241cb
gavin771 <gavin.samuels10@gmail.com>
2018-03-11 21:25:22 -0700

    added auth routes

---
+++ routes/auth.js
@@ -0,0 +1,24 @@
+const passport = require('passport');
+
+module.exports = (app) => {
+  const scopes = [
+    'https://www.googleapis.com/auth/spreadsheets',
+    'email',
+    'profile',
+    'https://www.googleapis.com/auth/drive'
+  ]
+  app.get('/auth/google', passport.authenticate('google', { scope: scopes }));
+
+  app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
+    res.redirect('/dashboard')
+  });
+
+  app.get('/auth/logout', (req, res) => {
+    req.logout();
+    res.redirect('/')
+  });
+
+  app.get('/api/me', (req, res) => {
+    res.send(req.user);
+  });
+}
