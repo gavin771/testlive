249c300509ee51d2baaf5836321f12a5fd9fae8b
gavin771 <gavin.samuels10@gmail.com>
2018-03-17 12:51:19 -0700

    added prod server config

--- index.js
+++ index.js
@@ -23,4 +23,15 @@ require('./services/passport');
 require('./routes/auth')(app);
 require('./routes/user')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  //look in client/build for file that match unknown requests
+  app.use(express.static('client/build'));
+
+  //for all other routes, serve the index.html file
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 app.listen(process.env.PORT || 5000);
